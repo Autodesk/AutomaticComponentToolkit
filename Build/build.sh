b09f5703c5b6f878fbcff8038bf7fe184a9e4365
Martin Weismann <martin.weismann@autodesk.com>
2018-12-18 10:49:43 +0100

    Update build script

--- Build/build.sh
+++ Build/build.sh
@@ -6,19 +6,21 @@ cd "$basepath/../Source"
 
 Sources="actutils.go automaticcomponenttoolkit.go buildbindingcdynamic.go buildbindingcpp.go buildbindinggo.go buildbindingnode.go buildbindingpascal.go buildbindingpython.go buildimplementationcpp.go buildimplementationpascal.go componentdefinition.go componentdiff.go languagewriter.go languagec.go languagepascal.go schemavalidation.go"
 GOARCH="amd64"
-LDFLAGS=-ldflags "-s -w"
+LDFLAGS1=-ldflags
+LDFLAGS2="-s -w"
 CGO_ENABLED=1
 
 echo "Build act.exe"
 GOOS="windows"
-go build $LDFLAGS -o ../act.exe $Sources
+go build $LDFLAGS1 "$LDFLAGS2" -o ../act.exe $Sources || exit 1
 
 echo "Build act.linux"
 GOOS="linux"
-go build $LDFLAGS -o ../act.linux $Sources
+go build $LDFLAGS1 "$LDFLAGS2" -o ../act.linux $Sources || exit 1
 
 echo "Build act.darwin"
 GOOS="darwin"
-go build $LDFLAGS -o ../act.darwin $Sources
+go build $LDFLAGS1 "$LDFLAGS2" -o ../act.darwin $Sources || exit 1
 
 cd "$startingpath"
+ls -ahl ..
