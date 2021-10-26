f09f06fd6302a860f0ac7ac3a31b3d723da97d11
Andrii Anpilogov <andrii.anpilogov@autodesk.com>
2021-10-25 22:00:08 -0700

    cgo on Linux required linking with dl library

--- Source/buildbindinggo.go
+++ Source/buildbindinggo.go
@@ -533,6 +533,7 @@ func buildGoWrapper(component ComponentDefinition, w LanguageWriter) error {
 	w.Writeln("package %s", packageName)
 	w.Writeln("")
 	w.Writeln("/*")
+	w.Writeln("#cgo linux LDFLAGS: -ldl")
 	w.Writeln("#include \"%s_dynamic.cc\"", packageName)
 	w.Writeln("")
 	w.Writeln("%s_pvoid load%sLibrary (const char * pFileName)", component.NameSpace, component.NameSpace)
