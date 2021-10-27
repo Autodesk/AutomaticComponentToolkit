9bdf0802feda1124c6e73bace7388ad5c5cd9988
Andrii Anpilogov <andrii.anpilogov@autodesk.com>
2021-10-27 12:47:43 -0700

    Add CSharp example

--- Examples/Injection/build.sh
+++ Examples/Injection/build.sh
@@ -23,5 +23,5 @@ echo "Build and test bindings examples with C++ library"
 ./Calculation_component/Examples/CppDynamic/build.sh
 ./Calculation_component/Examples/Python/build.sh
 ./Calculation_component/Examples/Pascal/build.sh
-
+./Calculation_component/Examples/CSharp/build.sh
 echo "Build and test are done and successful"
