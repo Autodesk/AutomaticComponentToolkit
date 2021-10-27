45c5d1132b76a33737dea8c54591466b6d565ee4
Andrii Anpilogov <andrii.anpilogov@autodesk.com>
2021-10-26 22:04:45 -0700

    Update docker.sh helper script

--- Build/run-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-cd "$(dirname "$0")"
-
-docker run -it --rm -v $PWD/..:/data act-build /data/Build/build.sh
-docker run -it --rm -v $PWD/..:/data act-build /data/Examples/build.sh
