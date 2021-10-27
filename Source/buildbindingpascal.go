81ea4d24c2b84a80a3d155156ce0a0a79eb61e09
Andrii Anpilogov <andrii.anpilogov@autodesk.com>
2021-10-27 11:42:13 -0700

    Specialized PolymorphicFactories for FPC 3.0.4

--- Source/buildbindingpascal.go
+++ Source/buildbindingpascal.go
@@ -417,6 +417,12 @@ func buildDynamicPascalImplementation(component ComponentDefinition, w LanguageW
 	w.Writeln("    class function Make(Wrapper: T%sWrapper; Handle: T%sHandle): _T; static;", NameSpace, NameSpace)
 	w.Writeln("  end;")
 
+	for i := 0; i < len(component.Classes); i++ {
+		class := component.Classes[i]
+		fullClassName := fmt.Sprintf("T%s%s", strings.ToUpper(NameSpace), class.ClassName)
+		w.Writeln("  function T%sPolymorphicFactoryMake%s(Wrapper: T%sWrapper; Handle: T%sHandle): %s;", NameSpace, class.ClassName, NameSpace, NameSpace, fullClassName)
+	}
+
 	w.Writeln("")
 	w.Writeln("implementation")
 	w.Writeln("")
@@ -449,7 +455,14 @@ func buildDynamicPascalImplementation(component ComponentDefinition, w LanguageW
 	w.Writeln("    end;")
 	w.Writeln("    if Result = nil then Result := _B.Create(Wrapper, Handle);")
 	w.Writeln("  end;")
-
+	for i := 0; i < len(component.Classes); i++ {
+		class := component.Classes[i]
+		fullClassName := fmt.Sprintf("T%s%s", strings.ToUpper(NameSpace), class.ClassName)
+		w.Writeln("  function T%sPolymorphicFactoryMake%s(Wrapper: T%sWrapper; Handle: T%sHandle): %s;", NameSpace, class.ClassName, NameSpace, NameSpace, fullClassName)
+		w.Writeln("  begin")
+		w.Writeln("    Result := T%sPolymorphicFactory<%s, %s>.Make(Wrapper, Handle);", NameSpace, fullClassName, fullClassName)
+		w.Writeln("  end;")	
+	}
 	w.Writeln("")
 	w.Writeln("(*************************************************************************************************************************")
 	w.Writeln(" Exception implementation")
@@ -1067,7 +1080,11 @@ func writePascalClassMethodImplementation(method ComponentDefinitionMethod, w La
 				initCommands = append(initCommands, "H"+param.ParamName+" := nil;")
 				callFunctionParameters = callFunctionParameters + "H" + param.ParamName
 				resultCommands = append(resultCommands, fmt.Sprintf("  if Assigned(H%s) then", param.ParamName))
-				resultCommands = append(resultCommands, fmt.Sprintf("    Result := T%sPolymorphicFactory<T%s%s, T%s%s>.Make(%s, H%s);", theNameSpace, theNameSpace, theParamClass, theNameSpace, theParamClass, theWrapperInstance, param.ParamName))
+				if NameSpace == theNameSpace {
+					resultCommands = append(resultCommands, fmt.Sprintf("    Result := T%sPolymorphicFactory<T%s%s, T%s%s>.Make(%s, H%s);", theNameSpace, theNameSpace, theParamClass, theNameSpace, theParamClass, theWrapperInstance, param.ParamName))
+				} else {
+					resultCommands = append(resultCommands, fmt.Sprintf("    Result := T%sPolymorphicFactoryMake%s(%s, H%s);", theNameSpace, theParamClass, theWrapperInstance, param.ParamName))
+				}
 
 			default:
 				return fmt.Errorf("invalid method parameter type \"%s\" for %s.%s (%s)", param.ParamType, ClassName, method.MethodName, param.ParamName)
