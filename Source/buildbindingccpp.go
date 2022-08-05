935104c1311f03b410593b920cb069960a5a092a
Alexander Oster <alexanderoster@users.noreply.github.com>
2022-08-05 16:07:12 +0200

    Added Cpp Documentation creation

--- Source/buildbindingccpp.go
+++ Source/buildbindingccpp.go
@@ -170,9 +170,13 @@ func BuildBindingCppImplicit(component ComponentDefinition, outputFolder string,
 		}
 	}
 
-	err = BuildCCPPDocumentation(component, outputFolderDocumentation, ClassIdentifier)
-	if err != nil {
-		return err
+	if (outputFolderDocumentation != "") {
+
+		err = BuildCCPPDocumentation(component, outputFolderDocumentation, ClassIdentifier)
+		if err != nil {
+			return err
+		}
+		
 	}
 
 
@@ -1620,7 +1624,7 @@ func buildCppHeader(component ComponentDefinition, w LanguageWriter, NameSpace s
 
 // BuildBindingCppExplicit builds headeronly C++-bindings of a library's API in form of expliclty loaded function handles.
 func BuildBindingCppExplicit(component ComponentDefinition, outputFolder string, outputFolderExample string,
-	indentString string, ClassIdentifier string) error {
+	outputFolderDocumentation string, indentString string, ClassIdentifier string) error {
 	forceRecreation := false
 	ExplicitLinking := true
 	namespace := component.NameSpace
@@ -1685,6 +1689,15 @@ func BuildBindingCppExplicit(component ComponentDefinition, outputFolder string,
 		} else {
 			log.Printf("Omitting recreation of C++Dynamic example file \"%s\"", DynamicCPPCMake)
 		}
+	}
+	
+	if (outputFolderDocumentation != "") {
+
+		err = BuildCCPPDocumentation(component, outputFolderDocumentation, ClassIdentifier)
+		if err != nil {
+			return err
+		}
+		
 	}
 
 	return nil
