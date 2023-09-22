395ccbab2f776df466f9b884443375030ea3b6f4
Robert Goss <goss.robert@gmail.com>
2023-09-23 00:10:36 +0100

    Create initial files

--- Build/build.bat
+++ Build/build.bat
@@ -3,9 +3,7 @@ set startingDir="%CD%"
 set basepath="%~dp0"
 
 cd %basepath%\..\Source
-set Sources=actutils.go automaticcomponenttoolkit.go buildbindingccpp.go buildbindingccppdocumentation.go buildbindingcsharp.go buildbindinggo.go buildbindingnode.go buildbindingpascal.go buildbindingpython.go buildimplementationcpp.go buildbindingjava.go buildimplementationpascal.go componentdefinition.go componentdiff.go languagewriter.go languagec.go languagecpp.go languagepascal.go
-
-set GOOS=windows
+set Sources=actutils.go automaticcomponenttoolkit.go buildbindingccpp.go buildbindingcsharp.go buildbindinggo.go buildbindingnode.go buildbindingpascal.go buildbindingpython.go buildimplementationcpp.go buildimplementationpascal.go buildimplementationrust.go componentdefinition.go componentdiff.go languagewriter.go languagec.go languagecpp.go languagepascal.go
 set GOARCH=amd64
 echo "Build act.win64.exe"
 go build -o ..\act.win64.exe %Sources%
